a1e09610f3cfc78fba664a7db549e9ea7e9447fb
Ting Fang, Tsai <cycpe950609@gmail.com>
2024-02-12 21:04:18 +0800

    UPDATED: Fix Production build error

--- ts/storage.ts
+++ ts/storage.ts
@@ -4,9 +4,6 @@ import {
     createSlice,
     PayloadAction
 } from "@reduxjs/toolkit";
-import thunkMiddleware, { ThunkDispatch } from "redux-thunk";
-import logger from "redux-logger";
-import { JsonObjectExpression } from "typescript";
 import Mode from "./mode";
 
 const data = {};
